2de3c06793feb6db5bb85a5f4297d784852358c6
Izunobi Keside <kesideizunobi@gmail.com>
2025-10-24 21:02:25 +0100

    Fix: api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API = axios.create({
     baseURL:
         import.meta.env.MODE === "development"
-            ? "http://localhost:5000"
-            : "https://local-service-booker-api.onrender.com",
+            ? "http://localhost:5000/api"
+            : "https://local-service-booker-api.onrender.com/api",
 });
 
 export default API;
